246a97414b390b263d433db4b35e92a6e36b22d3
Mister Bamboo <nicolascnaud1989@gmail.com>
2023-01-30 20:09:58 -0500

    feat: Add AStar base implementation

---
+++ UnityBIJ3/Assets/Core/Algorithms/AStarPathFinding/IAStarGridInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathFinding
+{
+    public interface IAStarGridInfo
+    {
+        int Width { get; }
+        int Height { get; }
+
+        bool IsWalkable(int x, int y);
+    }
+}
