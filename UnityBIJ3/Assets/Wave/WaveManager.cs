f13b0b1c65015cf987279c50947bc4b2615499b2
armenbalian <raphaelfortin@hotmail.com>
2023-01-30 21:09:12 -0500

    wave count

--- UnityBIJ3/Assets/Wave/WaveManager.cs
+++ UnityBIJ3/Assets/Wave/WaveManager.cs
@@ -1,14 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
+    public event Action<int> WaveSpawned = delegate { };
+
     [SerializeField]
     GameManager gameManager;
 
     [SerializeField]
     List<Wave> waves = new List<Wave>();
+
+    int currentWaveIndex = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -29,7 +34,14 @@ public class WaveManager : MonoBehaviour
             if (wave.timeInSeconds == currentTimeInSeconds)
             {
                 wave.StartWave();
+                currentWaveIndex++;
+                WaveSpawned(currentWaveIndex);
             }
         }
+    }
+
+    public int GetWaveCount()
+    {
+        return waves.Count;
     }
 }
